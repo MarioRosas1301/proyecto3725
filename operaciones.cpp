612a1f22e55c00f98cd52128b509970111ff3ea1
Mario Rosas <igrosas@hotmail.com>
2021-05-25 20:16:30 -0500

    Cambios realizados en la funcion saludo

--- operaciones.cpp
+++ operaciones.cpp
@@ -10,6 +10,9 @@ int validanumero(){
     return numero;
 }
 void saludoprogramador(string nombre){
+    cout<<"Bienvenidos a mi proyecto"<<endl;
+    cout<<"Define si no eres un robot contestando la pregunta siguiente";
+    cout<<"Como se llama tu mascota";
     cout<<"Buenas tardes "<<nombre;
 }
 main(){
