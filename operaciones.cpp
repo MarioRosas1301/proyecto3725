5f5e3c7bb8a28ec4473c5f798db219b99791dd16
Mario Rosas <igrosas@hotmail.com>
2021-05-26 09:15:56 -0500

    Muestro el valor del numero ingresado

--- operaciones.cpp
+++ operaciones.cpp
@@ -12,12 +12,13 @@ int validanumero(){
 void saludoprogramador(string nombre){
     cout<<"Bienvenidos a mi proyecto"<<endl;
     cout<<"Define si no eres un robot contestando la pregunta siguiente";
-    cout<<"Como se llama tu mascota";
+    cout<<"\nComo se llama tu mascota";
     cout<<"Buenas tardes "<<nombre;
 }
 main(){
     int n = validanumero();
     saludoprogramador("Juan");
+    cout<<"El valor ingresado es: "<<n<<endl;
 
 
 }
